ab742ee25e67f5d6fe6a581fafb4fc7c71f21f75
川崎暑士 <kawasaki8910@gmail.com>
2018-08-24 17:18:13 +0900

    add pug

---
+++ gulpfile.js
@@ -0,0 +1,43 @@
+const gulp = require('gulp');
+const webpackStream = require('webpack-stream');
+
+const webpack = require('webpack');
+const webpackConfig = require('./webpack.config');
+const browserSync = require('browser-sync');
+const pug = require('gulp-pug');
+
+const DEST = './static';
+
+gulp.task('pug', () => {
+  return gulp.src(`src/pug/**/[!_]*.pug`)
+    .pipe(pug({
+      pretty: true,
+      basedir: `src/pug`
+    }))
+    .pipe(gulp.dest(`${DEST}`));
+});
+
+gulp.task('javascript', function() {
+  return webpackStream(webpackConfig, webpack)
+    .pipe(gulp.dest(DEST));
+});
+
+gulp.task('browser-sync', function() {
+  browserSync({
+      server: {
+        baseDir: DEST,
+      },
+      ghostMode: false,
+      open: false,
+  });
+
+  gulp.watch('./src/pug/**/*.pug', ['pug', 'reload']);
+  gulp.watch('./src/js/**/*.js', ['javascript', 'reload']);
+});
+
+gulp.task('reload', function () {
+    browserSync.reload();
+});
+
+gulp.task('build', ['javascript', 'pug']);
+gulp.task('default', ['build', 'browser-sync']);
