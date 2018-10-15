acb2efd3f4cf490d82a89c2b62ab760370128aed
川崎暑士 <kawasaki8910@gmail.com>
2018-10-15 09:54:36 +0900

    fix vue slide

--- static/webslides.js
+++ static/webslides.js
@@ -1,7 +1,7 @@
 /*!
  * Name: WebSlides
  * Version: 1.5.0
- * Date: 2018-10-11
+ * Date: 2018-10-15
  * Description: Making HTML presentations easy
  * URL: https://github.com/webslides/webslides#readme
  * Credits: @jlantunez, @LuisSacristan, @Belelros
