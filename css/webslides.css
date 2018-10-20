4ca2dc17af2889f467a7ecf384f72bb33b5f3e8a
川崎暑士 <kawasaki8910@gmail.com>
2018-10-21 00:33:49 +0900

    Webセキュリティアップデート

--- css/webslides.css
+++ css/webslides.css
@@ -1,7 +1,7 @@
 /*!
  * Name: WebSlides
  * Version: 1.5.0
- * Date: 2018-10-19
+ * Date: 2018-10-20
  * Description: Making HTML presentations easy
  * URL: https://github.com/webslides/webslides#readme
  * Credits: @jlantunez, @LuisSacristan, @Belelros
