80be8ecf7e50318ef652a8a1600e6688cc60f711
川崎暑士 <kawasaki8910@gmail.com>
2019-08-09 11:22:01 +0900

    add

--- js/webslides.js
+++ js/webslides.js
@@ -1,7 +1,7 @@
 /*!
  * Name: WebSlides
  * Version: 1.5.0
- * Date: 2019-08-09
+ * Date: 2019-07-30
  * Description: Making HTML presentations easy
  * URL: https://github.com/webslides/webslides#readme
  * Credits: @jlantunez, @LuisSacristan, @Belelros
